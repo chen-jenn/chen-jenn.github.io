65101f63bb38f98370561de23b9454062b63bc46
jennifer <jenn.chen@live.ca>
2018-05-27 18:43:23 -0700

    Remove and move some files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        notice me senpai 
+      </div>
+    );
+  }
+}
+
+export { App };
