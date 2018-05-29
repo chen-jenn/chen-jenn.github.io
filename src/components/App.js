5dfbf53e32dddebabcfb61a504118c6d2dcbf815
jennifer <jenn.chen@live.ca>
2018-05-28 18:55:11 -0700

    Remove unneeded commponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Game } from './Game';
+import { Grid } from './Grid';
 
 class App extends Component {
   render(){
     return(
-      <Game />
+      <Grid />
     );
   }
 }
